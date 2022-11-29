c358d9d79e54d164b4bbd6802a70abe01a740141
SchuSt92 <steffen.schulz1992@icloud.com>
2022-11-29 16:36:35 +0100

    Rezepte

---
+++ LocalFood/View/RezepteView.swift
@@ -0,0 +1,20 @@
+//
+//  RezepteView.swift
+//  LocalFood
+//
+//  Created by Steffen Schulz on 29.11.22.
+//
+
+import SwiftUI
+
+struct RezepteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RezepteView_Previews: PreviewProvider {
+    static var previews: some View {
+        RezepteView()
+    }
+}
