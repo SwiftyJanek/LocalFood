3cc4e9afc67b07541ddc1827fb4919c778622a86
SchuSt92 <steffen.schulz1992@icloud.com>
2022-11-29 17:04:41 +0100

    Commit

--- LocalFood/View/RezepteView.swift
+++ LocalFood/View/RezepteView.swift
@@ -2,54 +2,15 @@
 //  RezepteView.swift
 //  LocalFood
 //
-//  Created by Janek Behrens on 14.10.22.
+//  Created by Steffen Schulz on 29.11.22.
 //
 
 import SwiftUI
-import CoreData
 
 struct RezepteView: View {
-    let columns = [GridItem(.flexible()), GridItem(.flexible())]
-
     var body: some View {
-        GeometryReader { geometry in
-            NavigationStack {
-                VStack{
-                    
-                    ScrollView{
-                        
-                        /// Hier ändern
-                        
-                        
-                        HStack{
-                            Text("Rezepte").font(.title2).fontWeight(.bold).multilineTextAlignment(.center)
-                        }.padding(.top, 10)
-                        Text("Neue Rezepte").fontWeight(.bold).padding(.top, 10)
-                        let images = ["1","2"]
-                        ImageSlider(images: images).frame(height: 262).shadow(radius: 10)
-                        Text("Rezept der Woche").fontWeight(.bold).padding(.top,10)
-                        let images2 = ["3","4"]
-                        ImageSlider(images: images2).frame(height: 262).shadow(radius: 10)
-                        Text("Rezepte für Dich").fontWeight(.bold).padding(.top, 10)
-                        ImageSlider(images: images2).frame(height: 262).shadow(radius: 10)
-                        Text("Challanges").fontWeight(.bold).padding(.top, 10)
-                        ImageSlider(images: images).frame(height: 262).shadow(radius: 10)
-                        
-                        /// Hier Ende
-                        
-                        
-                    }.frame(width: geometry.size.width , height: geometry.size.height/1.10).border(Color.gray).background(Color.white)
-                    
-                    TabBar()
-                    
-                    
-                }
-            }.navigationBarBackButtonHidden(true)
-        }
-        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-    
-    
 }
 
 struct RezepteView_Previews: PreviewProvider {
