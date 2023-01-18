58017659eaa5cf57b223e5d440c98b9a3105b34c
SchuSt92 <steffen.schulz1992@icloud.com>
2023-01-18 15:12:57 +0100

    Commit new Files Challenge

---
+++ LocalFood/View/ChallengeHeader.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeHeader.swift
+//  LocalFood
+//
+//  Created by Steffen Schulz on 18.01.23.
+//
+
+import SwiftUI
+
+struct ChallengeHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChallengeHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeHeader()
+    }
+}
---
+++ LocalFood/View/ChallengeNordsee.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeNordsee.swift
+//  LocalFood
+//
+//  Created by Steffen Schulz on 18.01.23.
+//
+
+import SwiftUI
+
+struct ChallengeNordsee: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChallengeNordsee_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeNordsee()
+    }
+}
